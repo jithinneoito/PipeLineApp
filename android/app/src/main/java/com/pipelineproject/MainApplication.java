146bea4c660194c06e460d8c03809a8a73fe985b
jithinneoito <jithindas@neoito.com>
2023-07-20 19:53:32 +0530

    code changes

--- android/app/src/main/java/com/pipelineproject/MainApplication.java
+++ android/app/src/main/java/com/pipelineproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pipelineapp;
+package com.pipelineproject;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/PipeLineProjectTests/PipeLineProjectTests.m
+++ ios/PipeLineProjectTests/PipeLineProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PipeLineAppTests : XCTestCase
+@interface PipeLineProjectTests : XCTestCase
 
 @end
 
-@implementation PipeLineAppTests
+@implementation PipeLineProjectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
